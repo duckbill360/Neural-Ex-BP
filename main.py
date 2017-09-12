718096dff1b84eafbffb79bd820fafb7b05a39a9
Po-Wei Cheng <a821023a821023@yahoo.com.tw>
2017-09-12 14:15:49 +0800

    First Commitment

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 
 import polar_codes
 import numpy as np
+import tensorflow as tf
 
-############### PARAMETERS ###############
-Times = 10      # i7 -> 50 : 10'25
+############### Polar Codes PARAMETERS ###############
 N = 1024        # code length
 R = 0.5         # code rate
 epsilon = 0.45   # cross-over probability for a BEC
 SNR_in_db = 2.0
+######################################################
 
 Var = 1 / (2 * R * pow(10.0, SNR_in_db / 10.0))
 sigma = pow(Var, 1 / 2)
@@ -17,3 +18,6 @@ B_N = polar_codes.permutation_matrix(N)
 frozen_indexes = polar_codes.generate_frozen_set_indexes(N, R, epsilon)
 G = polar_codes.generate_G_N(N)
 
+if __name__ == '__main__':
+    
+    pass
