eee815fba901192a7ade115e8adfd1c171ed77f2
Po-Wei Cheng <duckbill360@users.noreply.github.com>
2017-09-18 13:15:24 +0800

    Initializing some variables

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import tensorflow as tf
 
 ############### Polar Codes PARAMETERS ###############
+iter_num = 2
 N = 1024        # code length
 R = 0.5         # code rate
 epsilon = 0.45   # cross-over probability for a BEC
@@ -20,4 +21,20 @@ G = polar_codes.generate_G_N(N)
 
 if __name__ == '__main__':
 
-    pass
+    x = tf.placeholder(tf.float64, shape=(N, ), name='x')
+    y = tf.placeholder(tf.float64, shape=(N, ), name='y')
+
+    alpha = tf.Variable(dtype=tf.float64, initial_value=tf.zeros((iter_num, N // 2, ), dtype=tf.float64), expected_shape=(iter_num, N // 2, ), name='alpha', trainable=True)
+    beta = tf.Variable(dtype=tf.float64, initial_value=tf.zeros((iter_num, N // 2, ), dtype=tf.float64), expected_shape=(iter_num, N // 2, ), name='beta', trainable=True)
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    alpha_val = sess.run(alpha)
+    beta_val = sess.run(beta)
+    print(alpha_val)
+    print(beta_val)
+
+    print(alpha)
+    print(beta)
+
+    sess.close()
