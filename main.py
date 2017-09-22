1a6c29e894549b4d5a8a192e994f1b39169eac95
Po-Wei Cheng <a821023a821023@yahoo.com.tw>
2017-09-22 17:07:34 +0800

    Stack all hidden layers in a list.

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import numpy as np
 import tensorflow as tf
 
 import os
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 ############### Polar Codes PARAMETERS ###############
 iter_num = 2
 N = 1024        # code length
 n = int(np.log2(N))
+layers_per_iter = 2 * n - 1
 R = 0.5         # code rate
 epsilon = 0.45   # cross-over probability for a BEC
 SNR_in_db = 2.0
@@ -25,26 +26,57 @@ G = polar_codes.generate_G_N(N)
 
 
 def forwardprop(x, alpha, beta):
-
     # L messages and R messages
+    hidden_layers = [tf.Variable(dtype=tf.float64, initial_value=tf.zeros((N, ), dtype=tf.float64),
+                                 name='hidden_layer_' + str(i), trainable=False)
+                     for i in range((2 * n - 1) * iter_num)]
     L = tf.Variable(dtype=tf.float64, initial_value=tf.zeros((iter_num * n, N,), dtype=tf.float64),
-                    expected_shape=(iter_num * n, N,), name='L', trainable=False)
-    R = tf.Variable(dtype=tf.float64, initial_value=tf.zeros((iter_num * n, N,), dtype=tf.float64),
-                    expected_shape=(iter_num * n, N,), name='R', trainable=False)
+                    name='L', trainable=False)
+    R = tf.Variable(dtype=tf.float64, initial_value=tf.zeros((iter_num * (n - 1), N,), dtype=tf.float64),
+                    name='R', trainable=False)
+    R_LLR = tf.Variable(dtype=tf.float64, initial_value=tf.zeros((N, ), dtype=tf.float64),
+                        name='R_LLR', trainable=False)
+    y_hat = tf.Variable(dtype=tf.float64, initial_value=tf.zeros((N, ), dtype=tf.float64),
+                        name='y_hat', trainable=False)
 
-    for k in range(iter_num):
-        # L Propagation
-        for i in range(iter_num * n + 10):
-            for j in range(N // 2):
-                pass
-                # TODO
+    # k iterations
+    # for k in range(iter_num):
+    #     # R propagation first, L propagation later
+    #     print('iter_num', k)
+    #     # R propagation
+    #     print('R')
+    #     for i in range(k * layers_per_iter, k * layers_per_iter + (n - 1)):
+    #         print(i)
+    #         for j in range(N // 2):
+    #             pass
+    #             # TODO
+    #
+    #     # L propagation
+    #     print('L')
+    #     for i in range(k * layers_per_iter + (n - 1), (k + 1) * layers_per_iter):
+    #         print(i)
+    #         for j in range(N // 2):
+    #             pass
+    #             # TODO
 
-    print(L)
-    print(R)
+    # Run through all hidden layers.
+    # for i in range(len(hidden_layers)):
+    #     indicator = i % (2 * n - 1)
+    #     if indicator < (n - 1):     # -----------> former half: R propagation
+    #         pass
+    #     elif indicator >= (n - 1):  # -----------> later half: L propagation
+    #         pass
+
+    for i in hidden_layers:
+        print(i)
+
+    # Return a length-N vector
+    return y_hat
 
 
 def f():
     return tf.sign(x) * tf.sign(y) * tf.minimum(tf.abs(x), tf.abs(y))
+
 
 if __name__ == '__main__':
 
@@ -60,14 +92,12 @@ if __name__ == '__main__':
     beta = tf.Variable(dtype=tf.float64, initial_value=tf.zeros((iter_num, N // 2, ), dtype=tf.float64),
                        expected_shape=(iter_num, N // 2, ), name='beta', trainable=True)
 
+    # y_hat is a length-N vector.
     y_hat = forwardprop(x, alpha, beta)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         alpha_val = sess.run(alpha)
         beta_val = sess.run(beta)
-
-        print(alpha)
-        print(beta)
 
     sess.close()
